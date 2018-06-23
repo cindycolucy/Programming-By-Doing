1aaacca5ce193809b34de0c850778eff82e4c28b
Cindy Colucy <ckcolucy@gmail.com>
2018-06-23 15:37:21 -0400

    More

--- src/MoreVariablesAndPrinting.java
+++ src/MoreVariablesAndPrinting.java
@@ -2,26 +2,30 @@ public class MoreVariablesAndPrinting {
 
     public static void main( String[] args )
     {
-        String myName, myEyes, myTeeth, myHair;
-        int myAge, myHeight, myWeight;
+        String name, eyes, teeth, hair;
+        int age, height, weight;
 
-        myName = "Zed A. Shaw";
-        myAge = 35;     // not a lie
-        myHeight = 74;  // inches
-        myWeight = 180; // lbs
-        myEyes = "Blue";
-        myTeeth = "White";
-        myHair = "Brown";
+        name = "Zed A. Shaw";
+        age = 35;     // not a lie
+        height = 74;  // inches
+        weight = 180; // lbs
+        eyes = "Blue";
+        teeth = "White";
+        hair = "Brown";
 
-        System.out.println( "Let's talk about " + myName + "." );
-        System.out.println( "He's " + myHeight + " inches tall." );
-        System.out.println( "He's " + myWeight + " pounds heavy." );
+        System.out.println( "Let's talk about " + name + "." );
+        System.out.println( "He's " + height + " inches tall." );
+        System.out.println( "He's " + weight + " pounds heavy." );
         System.out.println( "Actually, that's not too heavy." );
-        System.out.println( "He's got " + myEyes + " eyes and " + myHair + " hair." );
-        System.out.println( "His teeth are usually " + myTeeth + " depending on the coffee." );
+        System.out.println( "He's got " + eyes + " eyes and " + hair + " hair." );
+        System.out.println( "His teeth are usually " + teeth + " depending on the coffee." );
 
         // This line is tricky; try to get it exactly right.
-        System.out.println( "If I add " + myAge + ", " + myHeight + ", and " + myWeight
-                + " I get " + (myAge + myHeight + myWeight) + "." );
+        System.out.println( "If I add " + age + ", " + height + ", and " + weight
+                + " I get " + (age + height + weight) + "." );
+
+        double kg = 50;
+        double pound = kg * 2.20462;
+        System.out.println(pound);
     }
 }
