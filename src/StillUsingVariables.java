b0ff068d0ba5e651ba5b8820856848a88259a81e
Cindy Colucy <ckcolucy@gmail.com>
2018-07-04 12:02:58 -0400

    1987

---
+++ src/StillUsingVariables.java
@@ -0,0 +1,10 @@
+public class StillUsingVariables {
+
+    public static void main(String[] args) {
+
+        String name = "Cindy Colucy";
+        int year = 1987;
+
+        System.out.println("My name is " + name + " and I graduated in the awesome year of " + year + ".");
+    }
+}
