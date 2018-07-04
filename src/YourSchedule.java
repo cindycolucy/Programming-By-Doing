676579da913c37167d6ce9abbbe7b24e400845f2
Cindy Colucy <ckcolucy@gmail.com>
2018-07-04 12:27:33 -0400

    schedule

---
+++ src/YourSchedule.java
@@ -0,0 +1,26 @@
+public class YourSchedule {
+
+    public static void main(String[] args) {
+
+        String class1 = "Gym";
+        String class2 = "English";
+        String class3 = "Social Studies";
+        String class4 = "Science";
+        String class5 = "Math";
+
+        String teacher1 = "Mr Jones";
+        String teacher2 = "Mrs Montgomery";
+        String teacher3 = "Mr Finn";
+        String teacher4 = "Mrs Palmer";
+        String teacher5 = "Mr Gruber";
+
+        System.out.println("+--------------------------------------------------+");
+        System.out.println("| 1 |               " + class1 + "|               " + teacher1 + "|");
+        System.out.println("| 2 |               " + class2 + "|           " + teacher2 + "|");
+        System.out.println("| 3 |               " + class3 + "|    " + teacher3 + "|");
+        System.out.println("| 4 |               " + class4 + "|           " + teacher4 + "|");
+        System.out.println("| 5 |               " + class5 + "|              " + teacher5 + "|");
+        System.out.println("+--------------------------------------------------+");
+
+    }
+}
