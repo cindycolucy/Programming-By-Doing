6fe96acaee4d4437614f464510a0879b1dd887df
Cindy Colucy <ckcolucy@gmail.com>
2018-07-04 11:51:01 -0400

    hope this works

---
+++ src/UsingVariables.java
@@ -0,0 +1,18 @@
+import java.sql.SQLOutput;
+
+public class UsingVariables {
+
+    public static void main(String[] args) {
+
+    int bob = 113;
+    double mary = 2.71828;
+    String word = "Computer Science";
+
+        System.out.println("The room # is " + bob);
+        System.out.println("e is close to " + mary);
+        System.out.println("I am learning a bit about " + word);
+
+
+}
+
+}
