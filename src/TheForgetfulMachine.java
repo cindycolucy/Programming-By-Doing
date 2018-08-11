9f0638202f534ab7d18c8dc1f82d4fd54bb9e5e6
Cindy Colucy <ckcolucy@gmail.com>
2018-08-11 13:41:17 -0400

    forgetful machine

---
+++ src/TheForgetfulMachine.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class TheForgetfulMachine {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Give me a word...");
+        scanner.next();
+        System.out.println("Give me another word...");
+        scanner.next();
+        System.out.println("Give me a number...");
+        scanner.next();
+        System.out.println("Give me another number...");
+        scanner.next();
+        System.out.println("Wow! Fascinating!");
+
+    }
+
+}
