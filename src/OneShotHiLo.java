a0aac499654c93e73646e2e1ecfbb943ee3d1a10
Cindy Colucy <ckcolucy@gmail.com>
2018-09-16 17:58:23 -0400

    One Shot Hi Lo

--- src/OneShotHiLo.java
+++ src/OneShotHiLo.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
+import java.util.Random;
+import java.util.Scanner;
 
 public class OneShotHiLo {
+
+    public static void main(String[] args) {
+
+        Random r = new Random();
+        Scanner scanner = new Scanner(System.in);
+
+        int guess;
+        int number = 1 + r.nextInt(100);
+
+        System.out.println("Guess a number 1-100.");
+        guess = scanner.nextInt();
+
+        if(guess == number){
+            System.out.println("Correct! You win!");
+        }
+        else if(guess < number){
+            System.out.println("Sorry, too low. The number was " + number + ".");
+        }
+        else System.out.println("Sorry, too high. The number was " + number + ".");
+
+
+    }
 }
