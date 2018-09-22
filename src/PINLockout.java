b83558188b288dd7074a3eef4bcda191531d3368
Cindy Colucy <ckcolucy@gmail.com>
2018-09-22 15:25:41 -0400

    PIN Lockout Extra Credit

--- src/PINLockout.java
+++ src/PINLockout.java
@@ -1,4 +1,30 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class PINLockout {
+
+    public static void main( String[] args )
+    {
+        Scanner keyboard = new Scanner(System.in);
+        int pin = 12345;
+        int tries = 0;
+
+        System.out.println("WELCOME TO THE BANK OF MITCHELL.");
+        System.out.print("ENTER YOUR PIN: ");
+        int entry = keyboard.nextInt();
+        int maxTries = 4;
+        tries++;
+
+        while ( entry != pin && tries < maxTries )
+        {
+            System.out.println("\nINCORRECT PIN. TRY AGAIN.");
+            System.out.print("ENTER YOUR PIN: ");
+            entry = keyboard.nextInt();
+            tries++;
+        }
+
+        if ( entry == pin )
+            System.out.println("\nPIN ACCEPTED. YOU NOW HAVE ACCESS TO YOUR ACCOUNT.");
+        else if ( tries >= maxTries )
+            System.out.println("\nYOU HAVE RUN OUT OF TRIES. ACCOUNT LOCKED.");
+    }
 }
