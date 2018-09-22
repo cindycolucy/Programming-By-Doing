489f0df606848c4e5d7f2b9f35b476136f65feaa
Cindy Colucy <ckcolucy@gmail.com>
2018-09-22 15:16:25 -0400

    Counting with a while Loop Extra Credit

--- src/CountingWithAWhileLoop.java
+++ src/CountingWithAWhileLoop.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class CountingWithAWhileLoop {
+    public static void main( String[] args )
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println( "Type in a message, and I'll display it five times." );
+        System.out.print( "Message: " );
+        String message = scanner.nextLine();
+
+        int n = 0;
+        while ( n < 100 )
+        {
+            System.out.println( (n+10) + ". " + message );
+            n+=10;
+        }
+
+    }
 }
