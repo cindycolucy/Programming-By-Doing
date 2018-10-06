550dd7ff135a2d32da9ac206588afe837a121444
Cindy Colucy <ckcolucy@gmail.com>
2018-10-06 18:57:56 -0400

    Counting By Halves

---
+++ src/CountingByHalves.java
@@ -0,0 +1,9 @@
+public class CountingByHalves {
+
+    public static void main(String[] args) {
+
+        for(double x = -10; x <= 10; x = x + .5){
+            System.out.println(x);
+        }
+    }
+}
