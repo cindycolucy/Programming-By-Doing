d05dc116279b032abee7bd7979a6bb5a1b65c6d3
Cindy Colucy <ckcolucy@gmail.com>
2018-10-07 12:30:39 -0400

    For Loop Challenge

---
+++ src/ForLoopChallenge.java
@@ -0,0 +1,11 @@
+public class ForLoopChallenge {
+
+    public static void main(String[] args) {
+
+        // Write a for loop that makes the variable j go from 15 to 30, counting by 3s
+
+        for(int i = 15; i <= 30; i = i + 3){
+            System.out.println(i);
+        }
+    }
+}
