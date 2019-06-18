d003713cc6b6ae9100ff2473385c2cff7101ed8f
Cindy Colucy <ckcolucy@gmail.com>
2019-06-18 15:52:24 -0400

    More Number Puzzles solved

--- src/MoreNumberPuzzles.java
+++ src/MoreNumberPuzzles.java
@@ -1,29 +1,63 @@
+import java.util.Scanner;
+
 public class MoreNumberPuzzles {
 
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int selection;
+        do {
+            System.out.println("Please choose an option...");
+            System.out.println("1) Find two digit numbers <= 56 with sums of digits > 10");
+            System.out.println("2) Find two digit number minus number reversed which equals sum of digits");
+            System.out.println("3) Quit");
+
+            selection = scanner.nextInt();
+
+            int sum = 0;
+            int num = 0;
+
+            if (selection == 1) {
+                fifty_six(sum);
+            }
+            if (selection == 2) {
+                number_puzzle(num);
+            }
+        }
+        while (selection != 3);
+    }
+
+    public static int fifty_six(int sum) {
 
         for (int a = 10; a <= 56; a++) {
 
             int b = a % 10;
             int c = a / 10;
-            int sum = b + c;
-            if(sum > 10){
-               System.out.println(a);
+            sum = b + c;
+            if (sum > 10) {
+                System.out.println(a);
 
-          }
-       }
-        System.out.println("*******************");
+            }
 
-        for (int num = 10; num <= 99; num++){
+        }
+        return sum;
+    }
+
+    public static int number_puzzle(int num) {
+
+        for (num = 10; num <= 99; num++) {
 
             int ones = num % 10;
             int tens = num / 10;
             int sum = ones + tens;
             int reverse = (ones * 10) + tens;
             int diff = num - reverse;
-            if(sum == diff){
+            if (sum == diff) {
                 System.out.println(num);
             }
         }
+        return num;
     }
+
 }
