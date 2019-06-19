c4e35d6bfc7a47d4f101ee9393d6a71a63401388
Cindy Colucy <ckcolucy@gmail.com>
2019-06-19 14:45:32 -0400

    Simple File Input

--- src/SimpleFileInput.java
+++ src/SimpleFileInput.java
@@ -1,2 +1,18 @@
+import java.io.File;
+import java.util.Scanner;
+
 public class SimpleFileInput {
+
+    public static void main( String[] args ) throws Exception
+    {
+
+        Scanner fileIn = new Scanner(new File("name.txt"));
+        String f_name =fileIn.next();
+        String l_name =fileIn.next();
+
+
+        System.out.println( "Using my psychic powers aided by reading data from the file, I have" );
+        System.out.println( "determined that your name is " +f_name+" "+l_name+"." );
+        System.out.println( "Amazing, huh?" );
+    }
 }
