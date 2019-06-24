e69ae2a4f19a74c4bd12f1a8ce29de6d1e0450e0
Cindy Colucy <ckcolucy@gmail.com>
2019-06-24 17:04:43 -0400

    I give up

--- src/WhereIsIt.java
+++ src/WhereIsIt.java
@@ -1,35 +1,36 @@
 import java.util.Random;
 import java.util.Scanner;
 
-public class IsItThereOrNot {
+public class WhereIsIt {
 
     public static void main(String[] args){
+
         Scanner keyboard = new Scanner(System.in);
         Random r = new Random();
-        int a[] = new int[10];
 
-        boolean found = false;
+        int a[] = new int[10];
+        int slot=0;
+        boolean found=false;
+
         System.out.print("Array: ");
         for(int i = 0; i < a.length; i++){
             a[i] = 1 + r.nextInt(50);
             System.out.print(a[i] + "  ");
         }
-        System.out.println();
-        System.out.print("Enter a number: ");
+        System.out.print("\nValue to find: ");
         int value = keyboard.nextInt();
-        System.out.println();
-
-        for ( int i = 0; i < a.length; i++ )
-        {
-            if ( value == a[i] && ! found )
+        for(int j = 0; j < a.length; j++){
+            if(a[j] == value && !found){
+                slot = j;
                 found = true;
-
+            }
+        }System.out.println();
+        if(found){
+            System.out.println(value + " is in slot " + slot + ".");
+        }else{
+            System.out.println(value + " is not in the array.");
         }
+    }
 
-        if (found)
-            System.out.println( value + " is in the array." );
-        else
-            System.out.println( value + " is not in the array." );
 
     }
-}
