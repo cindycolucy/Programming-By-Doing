0306257c89432b8ca1e16372d0fe2f027a132d3c
Cindy Colucy <ckcolucy@gmail.com>
2019-06-24 11:23:28 -0400

    Basic Arrays 1

--- src/BasicArrays1.java
+++ src/BasicArrays1.java
@@ -3,12 +3,9 @@ import java.util.ArrayList;
 public class BasicArrays1 {
 
     public static void main(String[] args){
-
-        ArrayList listTest = new ArrayList();
-        listTest.add( "-113" );
-        for(int i=0; i<10; i++){
-            System.out.println( "Slot " + i + " contains a " + listTest.get(0) );
+        int a[] = new int[10];
+        for(int i = 0; i < a.length; i++){
+            System.out.println("Slot " + i + " contains a -113");
         }
-
     }
 }
