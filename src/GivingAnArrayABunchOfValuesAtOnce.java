95720e4bdd77fd6c78230c3bcd8207ece7831407
Cindy Colucy <ckcolucy@gmail.com>
2019-06-25 15:20:55 -0400

    Giving an array a bunch of values at once

---
+++ src/GivingAnArrayABunchOfValuesAtOnce.java
@@ -0,0 +1,26 @@
+public class GivingAnArrayABunchOfValuesAtOnce {
+
+    public static void main( String[] args )
+    {
+        String[] arr1 = { "alpha", "bravo", "charlie", "delta", "epsilon" };
+
+        System.out.print("The first array is filled with the following values:\n\t");
+
+        for ( int i = 0; i < arr1.length; i++ )
+
+            System.out.print( arr1[i] + " " );
+
+        System.out.println();
+        System.out.println();
+
+        int[] arr2 = {1, 2, 9, 3, 6};
+
+        System.out.print("The second array is filled with the following values:\n\t");
+
+        for (int j = 0; j < arr2.length; j++){
+
+            System.out.print(arr2[j] + " ");
+        }
+
+    }
+}
