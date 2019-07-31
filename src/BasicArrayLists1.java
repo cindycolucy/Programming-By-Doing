646c4a9ca0f36f09e54bafd0bd2eba1f3d69fcef
Cindy Colucy <ckcolucy@gmail.com>
2019-07-31 09:49:56 -0400

    Basic Array Lists 1

--- src/BasicArrayLists1.java
+++ src/BasicArrayLists1.java
@@ -4,19 +4,14 @@ public class BasicArrayLists1 {
 
     public static void main(String[] args) {
 
-        ArrayList num = new ArrayList();
+        ArrayList<Integer> num = new ArrayList<Integer>();
 
         num.add(-113);
 
-        System.out.println("Slot 0 contains a " + num.get(0));
-        System.out.println("Slot 1 contains a " + num.get(0));
-        System.out.println("Slot 2 contains a " + num.get(0));
-        System.out.println("Slot 3 contains a " + num.get(0));
-        System.out.println("Slot 4 contains a " + num.get(0));
-        System.out.println("Slot 5 contains a " + num.get(0));
-        System.out.println("Slot 6 contains a " + num.get(0));
-        System.out.println("Slot 7 contains a " + num.get(0));
-        System.out.println("Slot 8 contains a " + num.get(0));
-        System.out.println("Slot 9 contains a " + num.get(0));
+        for (int i = 0; i < 10; i++){
+
+            System.out.println("Slot " + i + " contains a " + num.get(0));
+
+        }
     }
 }
